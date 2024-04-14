25ab7134ee57dbd21cc18edff68ac1f1b6c7f201
Yadnesh <yadneshmkombe@gmail.com>
2024-04-14 23:20:06 +0530

    AddedTest

--- src/test/java/com/simplilearn/Phase3Project2RestAssured/CreateUser.java
+++ src/test/java/com/simplilearn/Phase3Project2RestAssured/CreateUser.java
@@ -31,5 +31,26 @@ public class CreateUser {
 		System.out.println("The response code is :" +response.getStatusCode());
 		System.out.println("The response body is :" +response.body().asString());
 	}
+	
+	@Test
+	public void test2()
+	{	
+		HashMap<String, String> obj = new HashMap<String, String>();
+		
+		obj.put("name", "Samuel");
+		obj.put("job", "Leader");
+	
+		RestAssured.baseURI = "https://reqres.in/api/users";
+		
+		RequestSpecification  request = RestAssured.given();
+		
+		Response response = request.contentType(ContentType.JSON)
+									.accept(ContentType.JSON)
+									.body(obj)
+									.post();
+		
+		System.out.println("The response code is :" +response.getStatusCode());
+		System.out.println("The response body is :" +response.body().asString());
+	}
 
 }
